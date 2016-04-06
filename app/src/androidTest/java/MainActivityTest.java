b8250fb1ab8f1b069984226dd3f3b245bdc26a5a
Luke Meszar <lume0018@colorado.edu>
2016-04-06 11:33:46 -0600

    Added more tests

--- app/src/androidTest/java/MainActivityTest.java
+++ app/src/androidTest/java/MainActivityTest.java
@@ -18,11 +18,13 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.core.IsNull.notNullValue;
+import static org.hamcrest.Matchers.not;
+
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
@@ -38,15 +40,48 @@ public class MainActivityTest {
     }
 
     @Test
-    public  void sportsButtonShouldGetQuote() {
+    public void sportsButtonShouldGetQuote() {
         onView(withId(R.id.button3)).perform(click());
         onView(withId(R.id.quote)).check(matches(isDisplayed()));
     }
 
     @Test
-    public  void educationalButtonShouldGetQuote() {
-        onView(withId(R.id.button2)).perform(click());
-        onView(withId(R.id.quote)).check(matches(isDisplayed()));
+        public void educationalButtonShouldGetQuote() {
+            onView(withId(R.id.button2)).perform(click());
+            onView(withId(R.id.quote)).check(matches(isDisplayed()));
+        }
+
+    @Test
+    public void generalClickHidesButtons(){
+        onView(withId(R.id.button)).perform(click());
+        onView(withId(R.id.button)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.button2)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.button3)).check(matches(not(isDisplayed())));
     }
 
+    @Test
+    public void educationalClickHidesButtons(){
+        onView(withId(R.id.button2)).perform(click());
+        onView(withId(R.id.button)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.button2)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.button3)).check(matches(not(isDisplayed())));
+    }
+
+    @Test
+    public void sportsClickHidesButtons(){
+        onView(withId(R.id.button3)).perform(click());
+        onView(withId(R.id.button)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.button2)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.button3)).check(matches(not(isDisplayed())));
+    }
+
+    @Test
+    public void moreQuotesTest(){
+        onView(withId(R.id.button)).perform(click());
+        onView(withId(R.id.button4)).perform(click());
+        onView(withId(R.id.button)).check(matches(isDisplayed()));
+        onView(withId(R.id.button2)).check(matches(isDisplayed()));
+        onView(withId(R.id.button3)).check(matches(isDisplayed()));
+        onView(withId(R.id.quote)).check(matches(not(isDisplayed())));
+    }
 }
