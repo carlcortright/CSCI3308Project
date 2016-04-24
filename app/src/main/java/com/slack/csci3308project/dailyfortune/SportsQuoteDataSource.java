1fc91bda5db30bcbf0255fc39ee17df66f94ebe0
John Zavidniak <johnza459@gmail.com>
2016-04-24 13:51:01 -0600

    Added javadocs to SportsQuote.java and SportsQuoteDatasource.java

--- app/src/main/java/com/slack/csci3308project/dailyfortune/SportsQuoteDataSource.java
+++ app/src/main/java/com/slack/csci3308project/dailyfortune/SportsQuoteDataSource.java
@@ -20,19 +20,33 @@ public class SportsQuoteDataSource {
     private DatabaseHelper dbHelper;
     private String[] allColumns = {DatabaseHelper.SPORTS_COLUMN_ID, DatabaseHelper.SPORTS_QUOTE, DatabaseHelper.SPORTS_AUTHOR};
 
+    /**
+     * Construct this class by setting the construct to context.
+     * @param context The context of the main application.
+     */
     public SportsQuoteDataSource(Context context){
         dbHelper = new DatabaseHelper(context);
     }
 
+    /**
+     * Open a connection to the database if it exists
+     * @throws SQLException If the database cannot be opened
+     */
     public void open() throws SQLException{
         dbHelper.databaseExist();
         database = dbHelper.getReadableDatabase();
     }
 
+    /**
+     * Close the database
+     */
     public void close(){
         dbHelper.close();
     }
 
+    /**
+     * @return A List containing EducationalQuotes for all of our educational quotes
+     */
     public List<SportsQuote> getAllSportsQuotes(){
         List<SportsQuote> sportsQuotes = new ArrayList<SportsQuote>();
 
@@ -49,6 +63,9 @@ public class SportsQuoteDataSource {
 
     }
 
+    /**
+     * @return A random SportsQuote
+     */
     public SportsQuote getRandomSportsQuote(){
         SportsQuote sQuote = new SportsQuote();
 
@@ -70,6 +87,12 @@ public class SportsQuoteDataSource {
         return sQuote;
     }
 
+    /**
+     * Return a random number between min and max
+     * @param min the min number possible to return
+     * @param max the max number possible to return
+     * @return A number between min and max.
+     */
     public static int randInt(int min, int max) {
 
         // Usually this can be a field rather than a method variable
@@ -82,6 +105,12 @@ public class SportsQuoteDataSource {
         return randomNum;
     }
 
+    /**
+     * Set the cursor to a new quote.
+     * @param cursor the cursor to set
+     * @return The quote the cursor is at
+     * @throws SQLException If we cannot get the quote
+     */
     private SportsQuote cursorToQuote(Cursor cursor){
         SportsQuote sQuote = new SportsQuote();
         sQuote.setId(cursor.getInt(0));
