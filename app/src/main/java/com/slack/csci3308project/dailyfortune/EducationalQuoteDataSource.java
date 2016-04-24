f47067b680b8811258b137c5b9ffc56763dc7d33
John Zavidniak <johnza459@gmail.com>
2016-04-24 13:40:16 -0600

    Added GeneralQuote.java javadocs

--- app/src/main/java/com/slack/csci3308project/dailyfortune/EducationalQuoteDataSource.java
+++ app/src/main/java/com/slack/csci3308project/dailyfortune/EducationalQuoteDataSource.java
@@ -12,24 +12,41 @@ import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
+/**
+ * The source for our Educational Quotes
+ */
 public class EducationalQuoteDataSource {
     private SQLiteDatabase database;
     private DatabaseHelper dbHelper;
     private String[] allColumns = {DatabaseHelper.EDUCATIONAL_COLUMN_ID, DatabaseHelper.EDUCATIONAL_QUOTE, DatabaseHelper.EDUCATIONAL_AUTHOR};
 
+    /**
+     * Construct this class by setting the construct to context.
+     * @param context The context of the main application.
+     */
     public EducationalQuoteDataSource(Context context){
         dbHelper = new DatabaseHelper(context);
     }
 
+    /**
+     * Open a connection to the database if it exists
+     * @throws SQLException If the database cannot be opened
+     */
     public void open() throws SQLException{
         dbHelper.databaseExist();
         database = dbHelper.getReadableDatabase();
     }
 
+    /**
+     * Close the database
+     */
     public void close(){
         dbHelper.close();
     }
 
+    /**
+     * @return A List containing EducationalQuotes for all of our educational quotes
+     */
     public List<EducationalQuote> getAllEducationalQuotes(){
         List<EducationalQuote> educationalQuotes = new ArrayList<EducationalQuote>();
 
@@ -46,6 +63,9 @@ public class EducationalQuoteDataSource {
 
     }
 
+    /**
+     * @return A random EducationalQuote
+     */
     public EducationalQuote getRandomEducationalQuote(){
         EducationalQuote eQuote = new EducationalQuote();
 
@@ -67,7 +87,12 @@ public class EducationalQuoteDataSource {
         return eQuote;
     }
 
-
+    /**
+     * Return a random number between min and max
+     * @param min the min number possible to return
+     * @param max the max number possible to return
+     * @return A number between min and max.
+     */
     public static int randInt(int min, int max) {
 
         // Usually this can be a field rather than a method variable
@@ -81,6 +106,12 @@ public class EducationalQuoteDataSource {
     }
 
 
+    /**
+     * Set the cursor to a new quote.
+     * @param cursor the cursor to set
+     * @return The quote the cursor is at
+     * @throws SQLException If we cannot get the quote
+     */
     private EducationalQuote cursorToQuote(Cursor cursor) throws SQLException {
         EducationalQuote eQuote = new EducationalQuote();
         eQuote.setId(cursor.getInt(0));
