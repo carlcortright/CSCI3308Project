09a31a9d39714cc1aa96d10ad2e5aa7ac2883cd2
John Zavidniak <johnza459@gmail.com>
2016-04-24 13:48:25 -0600

    EducationalQuoteDatasource.java javadocs

--- app/src/main/java/com/slack/csci3308project/dailyfortune/GeneralQuoteDataSource.java
+++ app/src/main/java/com/slack/csci3308project/dailyfortune/GeneralQuoteDataSource.java
@@ -20,19 +20,33 @@ public class GeneralQuoteDataSource {
     private DatabaseHelper dbHelper;
     private String[] allColumns = {DatabaseHelper.GENERAL_COLUMN_ID, DatabaseHelper.GENERAL_QUOTE, DatabaseHelper.GENERAL_AUTHOR};
 
+    /**
+     * Construct this class by setting the construct to context.
+     * @param context The context of the main application.
+     */
     public GeneralQuoteDataSource(Context context){
         dbHelper = new DatabaseHelper(context);
     }
 
+    /**
+     * Open a connection to the database if it exists
+     * @throws SQLException If the database cannot be opened
+     */
      public void open() throws SQLException{
          dbHelper.databaseExist();
          database = dbHelper.getReadableDatabase();
     }
 
+    /**
+     * Close the database
+     */
     public void close(){
         dbHelper.close();
     }
 
+    /**
+     * @return A List containing EducationalQuotes for all of our educational quotes
+     */
     public List<GeneralQuote> getAllGeneralQuotes(){
         List<GeneralQuote> generalQuotes = new ArrayList<GeneralQuote>();
 
@@ -49,6 +63,9 @@ public class GeneralQuoteDataSource {
 
     }
 
+    /**
+     * @return A random General quote
+     */
     public GeneralQuote getRandomGeneralQuote(){
         GeneralQuote gQuote = new GeneralQuote();
 
@@ -70,7 +87,12 @@ public class GeneralQuoteDataSource {
         return gQuote;
     }
 
-
+    /**
+     * Return a random number between min and max
+     * @param min the min number possible to return
+     * @param max the max number possible to return
+     * @return A number between min and max.
+     */
     public static int randInt(int min, int max) {
 
         // Usually this can be a field rather than a method variable
@@ -83,6 +105,12 @@ public class GeneralQuoteDataSource {
         return randomNum;
     }
 
+    /**
+     * Set the cursor to a new quote.
+     * @param cursor the cursor to set
+     * @return The quote the cursor is at
+     * @throws SQLException If we cannot get the quote
+     */
     private GeneralQuote cursorToQuote(Cursor cursor){
         GeneralQuote gQuote = new GeneralQuote();
         gQuote.setId(cursor.getInt(0));
